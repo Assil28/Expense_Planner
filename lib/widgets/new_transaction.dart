ffc59fa7d629716f8ba77c68b92a9a049c16cf9e
Assil Dkhil <assil.dkhil28@gmail.com>
2024-03-07 13:14:56 +0100

    add file chart.dart

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,53 +1,72 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  
-        /*String titleInput;
-    String amountInput;*/
-    final titleInputController =TextEditingController();
-    final amountInputController =TextEditingController();
-    final Function addTx;
+class NewTransaction extends StatefulWidget {
+  final Function addTx;
 
-    NewTransaction(this.addTx);
+  NewTransaction(this.addTx);
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  /*String titleInput;
+    String amountInput;*/
+  final titleInputController = TextEditingController();
+
+  final amountInputController = TextEditingController();
+
+  void submitData() {
+    final enteredTitle = titleInputController.text;
+    final enteredAmount = double.parse(amountInputController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    } // lehna golna ken enteredTitle feregh wala enteredAmount <=0 il ne vas pas executer le code apres
+    // ma3neha l addTx mch bch yaamlelha execution
+    widget.addTx(enteredTitle, enteredAmount);
+
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    return    Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Title'),
-                    /*onChanged: (val) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              /*onChanged: (val) {
                       titleInput = val;
                     },*/
-                    controller: titleInputController,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Amount'),
-                    /*onChanged: (val) {
+              controller: titleInputController,
+              onSubmitted: (_) => submitData,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              /*onChanged: (val) {
                       amountInput = val;
                     },*/
-                    controller: amountInputController,
-                  ),
-                  ElevatedButton(
-                    child: Text(
-                      'Add Transaction',
-                      style: TextStyle(
-                        color: Colors.blue,
-                      ),
-                    ),
-                    onPressed: () {
-                      addTx(titleInputController.text,double.parse(amountInputController.text));
-                    },
-                  ),
-                ],
-              ),
+              controller: amountInputController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData,
             ),
-          );
+            ElevatedButton(
+              child: Text(
+                'Add Transaction',
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+              onPressed: submitData,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,53 +3,75 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
-
   final List<Transaction> transactions;
 
   TransactionList(this.transactions);
- 
 
   @override
   Widget build(BuildContext context) {
-    
-    return Column(
-            children: transactions.map((tx) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.purple, width: 2)),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$ ${tx.amount}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.purple),
-                        )),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '${tx.title}',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ],
+    return Container(
+      height: 500,
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'No Transactions added yet !',
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
-              );
-            }).toList(),
-          );
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                          margin: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Theme.of(context).primaryColor,
+                                  width: 2)),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            '\$ ${transactions[index].amount.toStringAsFixed(2)}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Theme.of(context).primaryColor),
+                          )),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${transactions[index].title}',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(transactions[index].date),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+              itemCount: transactions.length,
+            ),
+    );
   }
 }
