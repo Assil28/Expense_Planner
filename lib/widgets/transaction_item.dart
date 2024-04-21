02597cba330db2c9db6e3151052c0f65699c3211
Assil Dkhil <assil.dkhil28@gmail.com>
2024-04-21 22:37:57 +0100

    Using Keys

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     super.key,
     required this.transaction,
@@ -14,36 +16,56 @@ class TransactionItem extends StatelessWidget {
   final Function deleteTx;
 
   @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+late Color _bgColor;
+
+@override
+  void initState() {
+    const availleColors =[
+      Colors.red,
+      Colors.black,
+      Colors.blue,
+      Colors.purple,
+    ];
+    _bgColor=availleColors[Random().nextInt(4)];
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
       elevation: 5,
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30,
           child: Padding(
               padding: const EdgeInsets.all(10),
               child: FittedBox(
-                  child: Text('\$${transaction.amount}'))),
+                  child: Text('\$${widget.transaction.amount}'))),
         ),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: Theme.of(context).textTheme.titleLarge,
         ),
         subtitle: Text(
-          DateFormat.yMMMd().format(transaction.date),
+          DateFormat.yMMMd().format(widget.transaction.date),
           style: TextStyle(color: Colors.grey),
         ),
         trailing: MediaQuery.of(context).size.width > 460
             ? IconButton( //normalement na3mel TextButton ema me habetech tekhdem 
                 icon: const Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
               )
             : IconButton(
                 icon: const Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
               ),
       ),
     );
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -35,12 +35,11 @@ class TransactionList extends StatelessWidget {
               ],
             );
           })
-        : ListView.builder(
-            itemBuilder: (BuildContext context, int index) {
-              return TransactionItem(
-                  transaction: transactions[index], deleteTx: deleteTx);
-            },
-            itemCount: transactions.length,
+        : ListView(
+            children: transactions
+                .map((tx) => TransactionItem(
+                    key: ValueKey(tx.id), transaction: tx, deleteTx: deleteTx))
+                .toList(),
           );
   }
 }
